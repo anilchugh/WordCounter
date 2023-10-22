9f3609f1fcfa24e39e749412083011c29e047cb7
Anil Chugh <anil_k_chugh@yahoo.com>
2023-10-23 00:54:54 +0100

    Minor refactor

--- src/test/java/com/wordcounter/controller/WordCounterControllerUnitTest.java
+++ src/test/java/com/wordcounter/controller/WordCounterControllerUnitTest.java
@@ -1,18 +1,22 @@
-package com.wordounter.service;
+package com.wordcounter.controller;
 
 import com.wordcounter.WordCounterApplication;
+import com.wordcounter.model.WordCounterResponse;
+import com.wordcounter.service.WordCounterService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,20 +24,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest(classes = WordCounterApplication.class)
-public class WordCounterControllerIntegrationTest {
+public class WordCounterControllerUnitTest {
 
     @Autowired
     private MockMvc mvc;
 
-    @Test
-    public void countWordsTest() throws Exception {
-        mvc.perform(get("/wordcounter?words=the").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-                .andExpect(content().string(containsString("{\"the\":1}")));
+    @MockBean
+    private WordCounterService wordCounterService;
+
+    private WordCounterResponse prepareWordCounterResponse(String word, long count) {
+        WordCounterResponse wordCounterResponse = new WordCounterResponse();
+        wordCounterResponse.getWordCountMap().put(word, count);
+        return wordCounterResponse;
     }
 
     @Test
-    public void countWordsWithNonAlphanumericTest() throws Exception {
-        mvc.perform(get("/wordcounter?words=the@").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(HttpStatus.BAD_REQUEST.value())).andExpect(content().string(
-                is("{\"status\":400,\"message\":\"Words must be alphanumeric: the@\"}")));
+    public void countWordsTest() throws Exception {
+        when(wordCounterService.countWords(Mockito.anyList())).thenReturn(prepareWordCounterResponse("the", 1l));
+        mvc.perform(get("/wordcounter?words=the").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(content().string(containsString("{\"the\":1}")));
     }
 }
