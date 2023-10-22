3b76d578889c53e87ebdd82fea4cd701d136eff8
Anil Chugh <anil_k_chugh@yahoo.com>
2023-10-23 00:50:00 +0100

    Add unit test for validation error

--- src/test/java/com/wordounter/service/WordCounterControllerIntegrationTest.java
+++ src/test/java/com/wordounter/service/WordCounterControllerIntegrationTest.java
@@ -6,11 +6,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,5 +29,11 @@ public class WordCounterControllerIntegrationTest {
     public void countWordsTest() throws Exception {
         mvc.perform(get("/wordcounter?words=the").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
                 .andExpect(content().string(containsString("{\"the\":1}")));
+    }
+
+    @Test
+    public void countWordsWithNonAlphanumericTest() throws Exception {
+        mvc.perform(get("/wordcounter?words=the@").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(HttpStatus.BAD_REQUEST.value())).andExpect(content().string(
+                is("{\"status\":400,\"message\":\"Words must be alphanumeric: the@\"}")));
     }
 }
